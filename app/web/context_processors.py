4e2b3ac84ce5d4896de2b1fdfc8b854c75f2fac2
Mario Esteban Hernandez <hdez.marioe@gmail.com>
2020-08-20 12:33:10 -0500

    variables globales

--- app/web/context_processors.py
+++ app/web/context_processors.py
@@ -1,7 +1,14 @@
-from app.web.models import RedSocial
+from app.web.models import RedSocial, Chalet
 def load_red_socials(request):
     rs=RedSocial.objects.all()
     items={ 
         'redes_sociales':rs
+    }
+    return items
+
+def load_last_chalet(request):
+    chalets=Chalet.objects.all().order_by('-chalet_creado')[:6]
+    items={ 
+        'chalets_lasts':chalets
     }
     return items
